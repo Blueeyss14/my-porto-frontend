66cbee6a93a116c8766bff3f0efcf8e228ff1c6f
unknown <delkanoberutu8@gmail.com>
2025-08-04 03:51:34 +0700

    turu

--- src/view/components/MyProfile.jsx
+++ src/view/components/MyProfile.jsx
@@ -1,20 +1,24 @@
 const MyProfile = () => {
   return (
-    <div className="h-full w-full flex justify-between items-end overflow-hidden px-30">
+    <div className="h-full w-full flex justify-between items-end overflow-hidden px-30 [@media(max-width:1420px)]:px-10 [@media(max-width:800px)]:flex-col [@media(max-width:800px)]:items-center [@media(max-width:600px)]:px-3">
       <div className="h-[70%] flex-1 flex flex-col justify-center">
-        <h1 className="font-custom font-semibold text-6xl">I'm Delkano</h1>
-        <p className="pr-60 py-5 text-justify leading-8 ">
-          I'm a 2nd-year Information Technology student at Telkom University with a GPA of 3.59 and a strong passion for technology,
-including software development, design, music production, and 3D modeling and animation. Skilled in game development and
-creating interactive environments, I’m dedicated to continuous learning, improvement, and delivering quality results.
-        </p>
+        <h1 className="font-custom font-semibold text-6xl pr-5 [@media(max-width:500px)]:text-3xl [@media(max-width:600px)]:pr-0">I'm Delkano</h1>
+        <div className="pr-20 [@media(max-width:600px)]:pr-5">
+          <p className="text-justify pt-5 pb-1 ">
+            an Information Technology student at Telkom University
+          </p>
+          <p className="text-justify">
+            with a strong passion for technology and innovation.
+          </p>
+        </div>
+
         {/* <iframe
           src="https://www.youtube.com/embed/ewxmv2tyeRs?autoplay=1&mute=1"
           allow="autoplay"
         //   style={{ width: 0, height: 0, visibility: "hidden" }}
         /> */}
       </div>
-      <div className="h-[70%] w-120 bg-homeBg rounded-t-[20px] overflow-hidden">
+      <div className="h-[70%] w-120 bg-homeBg rounded-t-[20px] overflow-hidden [@media(max-width:1240px)]:h-[60%] [@media(max-width:600px)]:w-[100%]">
         <img src="assets/roblox.png" className="h-full w-full object-cover" />
       </div>
     </div>
--- src/view/pages/HomePage.jsx
+++ src/view/pages/HomePage.jsx
@@ -9,8 +9,8 @@ const HomePage = () => {
         className="object-cover h-full w-full"
         autoPlay
       />
-      <div className="w-screen h-screen inset-0 backdrop-blur-[60px] bg-white/10 absolute flex flex-col items-center justify-end px-[150px]">
-        <div className="bg-homeBg h-[80%] w-full rounded-t-[30px] overflow-hidden flex flex-col">
+      <div className="w-screen h-screen inset-0 backdrop-blur-[60px] bg-white/10 absolute flex flex-col items-center justify-end px-[150px] [@media(max-width:1050px)]:px-[20px]">
+        <div className="bg-homeBg h-[80%] w-full rounded-t-[30px] overflow-hidden flex flex-col [@media(max-width:1420px)]:h-[90%]">
           <div className="w-full flex justify-end pr-10 py-5">
             <h1 className="font-custom">Project</h1>
           </div>

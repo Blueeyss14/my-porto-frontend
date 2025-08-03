42e67aed4d066994af2224bf4043d8d0f87eefa8
unknown <delkanoberutu8@gmail.com>
2025-08-03 22:03:31 +0700

    init

---
+++ src/components/MediaPlayer.jsx
@@ -0,0 +1,15 @@
+const MediaPlayer = ({ src, className = "", autoPlay = true }) => {
+  const ext = src.split('.').pop().toLowerCase();
+
+  if (['png', 'jpg', 'jpeg', 'webp'].includes(ext)) {
+    return <img src={src} alt="media" className={className} />;
+  } else if (['mp4', 'webm'].includes(ext)) {
+    return (
+      <video src={src} controls autoPlay={autoPlay} loop muted className={className} />
+    );
+  } else {
+    return <p>Unsupported format</p>;
+  }
+};
+
+export default MediaPlayer

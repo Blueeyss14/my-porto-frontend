4562b2e1fcf713219896b848718037933d69685c
unknown <delkanoberutu8@gmail.com>
2025-08-05 23:10:11 +0700

    update

--- src/view/pages/HomePage.jsx
+++ src/view/pages/HomePage.jsx
@@ -17,18 +17,21 @@ const HomePage = () => {
       <div className="w-screen h-screen inset-0 backdrop-blur-[50px] bg-black/30 absolute overflow-hidden flex flex-col items-center justify-end">
         <div className="w-[90%] flex justify-end items-center">
           <BlurBackgorund
-            className="border-b-0 flex justify-between p-[6px] w-45 mb-4"
+            className="border-b-0 flex justify-between p-[6px] mb-4 pl-2"
             roundedClass="rounded-[30px]"
           >
             {medsosItems.map((item) => (
               // <h1 key={item.id}>{item.label}</h1>
-              <img src={`${item.icon}`} className="w-9 h-9 bg-medsos hover-medsos" />
+              <div className="flex items-center justify-center hover-medsos">
+              <img src={`${item.icon}`} className="w-8.5 h-8.5 bg-medsos" />
+              <h1 className="pr-5 pl-2">{item.label}</h1>
+              </div>
             ))}
             {/* <Navbar/> */}
           </BlurBackgorund>
         </div>
         <BlurBackgorund className="border-b-0 h-[85%] w-[90%]">
-          {/* <Navbar/> */}
+          <Navbar/>
         </BlurBackgorund>
         {/* <div className="bg-white/10 border-white/10 border-2 border-b-0 h-[85%] w-[90%] rounded-t-4xl backdrop-blur-[20px] overflow-hidden">
           <Navbar />

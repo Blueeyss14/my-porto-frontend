652a7971ba4a045ebd8c28580cd0cfac1b11c672
unknown <delkanoberutu8@gmail.com>
2025-08-08 05:46:58 +0700

    finish message and name input

--- src/view/pages/ProfilePage.jsx
+++ src/view/pages/ProfilePage.jsx
@@ -8,18 +8,18 @@ const ProfilePage = () => {
       className="flex-1 relative overflow-hidden object-cover flex items-center justify-center font-custom w-full"
       // style={{backgroundImage : "url('assets/bg.jpg')"}}
     >
-      <div className="w-[90%] h-full flex items-center  [@media(max-width:850px)]:items-start">
-        <div className="w-[50%] pr-5 flex flex-col [@media(max-width:850px)]:items-center [@media(max-width:850px)]:w-full [@media(max-width:850px)]:mt-10">
+      <div className="w-[90%] h-full flex items-center  [@media(max-width:850px)]:items-start [@media(max-width:500px)]:w-[80%]">
+        <div className="w-[50%] pr-5 flex flex-col [@media(max-width:850px)]:items-start [@media(max-width:850px)]:w-full [@media(max-width:850px)]:mt-10">
           <TypewriterLoop
             text="I'm Delkano"
             speed={100}
             className="font-custom font-medium text-homeBg2 text-5xl mb-5 [@media(max-width:1000px)]:text-4xl [@media(max-width:500px)]:text-2xl [@media(max-width:500px)]:mb-2"
           />
 
-          <h1 className="leading-8 [@media(max-width:1000px)]:leading-7 text-homeBg2/80 text-[1.2rem] [@media(max-width:1000px)]:text-[0.9rem] [@media(max-width:850px)]:text-center [@media(max-width:500px)]:text-[0.7rem] [@media(max-width:500px)]:leading-5">
+          <h1 className="leading-8 [@media(max-width:1000px)]:leading-7 text-homeBg2/80 text-[1.2rem] [@media(max-width:1000px)]:text-[0.9rem] [@media(max-width:850px)]:text-justify [@media(max-width:500px)]:text-[0.7rem] [@media(max-width:500px)]:leading-5">
             an Information Technology student at Telkom University
           </h1>
-          <h1 className="leading-8 [@media(max-width:1000px)]:leading-7 text-homeBg2/80 text-[1.2rem] [@media(max-width:1000px)]:text-[0.9rem]  [@media(max-width:850px)]:text-center [@media(max-width:500px)]:text-[0.7rem] [@media(max-width:500px)]:leading-5">
+          <h1 className="leading-8 [@media(max-width:1000px)]:leading-7 text-homeBg2/80 text-[1.2rem] [@media(max-width:1000px)]:text-[0.9rem]  [@media(max-width:850px)]:text-justify [@media(max-width:500px)]:text-[0.7rem] [@media(max-width:500px)]:leading-5">
             with a strong passion for technology and innovation.
           </h1>
         </div>

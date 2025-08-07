f18063d5ec3a6b3d6dd2f2113cb2de2c87766840
unknown <delkanoberutu8@gmail.com>
2025-08-08 05:12:24 +0700

    finish message and name input

--- src/view/components/NameInput.jsx
+++ src/view/components/NameInput.jsx
@@ -1,7 +1,9 @@
 import { useChatStore } from "../../state/chatStore";
 import { useState } from "react";
+import BlurBackgorund from "../shared/components/BlurBackgorund";
 
 export const NameInput = () => {
+  const { toggleIsClicked } = useChatStore();
   const { name, setName, message, postChat, setMessage } = useChatStore();
   const [isLoading, setIsLoading] = useState(false);
 
@@ -14,34 +16,53 @@ export const NameInput = () => {
       await postChat({ user: name, message });
       setName("");
       setMessage("");
+      
     } catch (err) {
       console.error("Failed to send message:", err);
     } finally {
+      toggleIsClicked(false)
       setIsLoading(false);
     }
   };
 
-  return <div>
-    
-  </div>;
-
-  // return (
-  //   <form onSubmit={handleSubmit} className="p-4">
-  //     <input
-  //       type="text"
-  //       value={name}
-  //       onChange={(e) => setName(e.target.value)}
-  //       placeholder="Your name"
-  //       className="w-full px-3 py-2 border rounded"
-  //       required
-  //     />
-  //     <button
-  //       type="submit"
-  //       disabled={isLoading}
-  //       className="mt-2 px-4 py-2 bg-blue-600 text-white rounded"
-  //     >
-  //       {isLoading ? 'Sending...' : 'Send'}
-  //     </button>
-  //   </form>
-  // );
+  return (
+    <div className="flex w-full items-center justify-end font-custom text-[1rem]">
+      <div className="w-full border-2 border-homeBg2/70 rounded-full px-5 py-1.5 mr-5">
+        <form className="whitee">
+          <input
+            // readOnly={isClicked}
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                e.preventDefault();
+                handleSubmit(e);
+              }
+            }}
+            className="w-full border-0 border-transparent focus:outline-none focus:ring-0 text-[0.9rem]"
+            placeholder="Your Name"
+          ></input>
+        </form>
+      </div>
+      <div>
+        {/* SEND BUTTON */}
+        <BlurBackgorund
+          onClick={handleSubmit}
+          background="bg-white/0 border-white/30 hover:bg-white/20 transition duration-200 ease-in-out"
+          className="w-11 h-11 flex items-center justify-center shadow-white/20 hover:shadow-[0_0_20px_white]"
+          roundedClass="rounded-full"
+        >
+          {isLoading ? (
+            <div className="w-5 h-5 border-2 border-t-transparent border-white rounded-full animate-spin"></div>
+          ) : (
+            <img
+              src="assets/icons/paper-plane.png"
+              className="w-[60%] h-[60%] ml-1 hover-medsos"
+            />
+          )}
+        </BlurBackgorund>
+      </div>
+    </div>
+  );
 };
--- src/view/pages/ConfirmMessageWindow.jsx
+++ src/view/pages/ConfirmMessageWindow.jsx
@@ -3,12 +3,13 @@ import { useChatStore } from "../../state/chatStore";
 
 const ConfirmMessageWindow = () => {
   const { isClicked, toggleIsClicked } = useChatStore();
+
   return (
     <div
       onClick={() => toggleIsClicked(false)}
       className={`
-    absolute w-screen h-screen bg-black/10 backdrop-blur-[30px] overflow-hidden z-50
-    transition-opacity duration-300 ease-in-out
+    absolute w-screen h-screen bg-black/10 backdrop-blur-[30px] overflow-hidden
+    transition-opacity duration-300 ease-in-out flex justify-center items-center
     ${
       isClicked
         ? "opacity-100 pointer-events-auto"
@@ -16,8 +17,16 @@ const ConfirmMessageWindow = () => {
     }
   `}
     >
-      <div onClick={(e) => e.stopPropagation()}>
-        <NameInput />
+      <div className="w-[90%] h-full flex flex-col justify-center items-center">
+        <h1 className="font-custom font-bold text-homeBg2 text-6xl text-center">
+          This is My Portfolio
+        </h1>
+        <h1 className="font-custom font-medium text-homeBg2 text-[1.2rem] text-center mt-1 mb-5">
+          Your message will appear, so kindly keep it positive.
+        </h1>
+        <div className="w-full max-w-100" onClick={(e) => e.stopPropagation()}>
+          <NameInput />
+        </div>
       </div>
     </div>
   );

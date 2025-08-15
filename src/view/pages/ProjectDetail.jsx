680afb6dd080555503208d20586164f33111cdba
unknown <delkanoberutu8@gmail.com>
2025-08-15 23:09:02 +0700

    update dulu mau tidur

--- src/view/pages/ProjectDetail.jsx
+++ src/view/pages/ProjectDetail.jsx
@@ -21,19 +21,31 @@ const ProjectDetail = () => {
   if (!project) return <div className="text-black p-5">Loading...</div>;
 
   return (
-    <div className="w-full h-full p-5 text-black flex flex-col items-center">
-      {project.image_url.map((img) => (
-        <img
-          src={img}
-          alt={project.title}
-          className="w-full max-w-lg h-auto mb-5 rounded-lg object-cover"
-        />
-      ))}
-
-      <h1 className="text-3xl font-bold">{project.title}</h1>
-      <h1 className="whitespace-pre-line text-justify">{project.description}</h1>
+    <div className="w-screen relative">
+      <div className="absolute w-full bg-amber-300 h-[300px]"></div>
+      <div className="absolute w-[500px] bg-red-300/20 h-[400px] transform translate-y-25">
+      </div>
     </div>
   );
-};
 
+  //   return (
+  //     <div className="w-full h-full p-5 text-black flex flex-col items-center px-10">
+  //       <div className="w-full h-full rounded-2xl overflow-hidden">
+  //       <img src={project.thumbnail} alt="" />
+
+  //       </div>
+  //       {project.image_url.map((img) => (
+  //         <img
+  //           src={img}
+  //           alt={project.title}
+  //           className="w-full max-w-lg h-auto mb-5 rounded-lg object-cover"
+  //         />
+  //       ))}
+
+  //       <h1 className="text-3xl font-bold">{project.title}</h1>
+  //       <h1 className="whitespace-pre-line text-justify">{project.description}</h1>
+  //     </div>
+  //   );
+  // };
+};
 export default ProjectDetail;

b333b5c45afe68c31522b9eeb178994e31fc9158
unknown <delkanoberutu8@gmail.com>
2025-08-25 23:31:54 +0700

    update loading handle

--- src/view/pages/ProjectDetail.jsx
+++ src/view/pages/ProjectDetail.jsx
@@ -22,20 +22,12 @@ const ProjectDetail = () => {
     }
   }, [projects, id]);
 
-  if (loading)
+  if (loading || !project)
     return (
       <div className="flex items-center justify-center h-screen">
         <div className="w-12 h-12 border-4 border-black border-dashed rounded-full animate-spin"></div>
       </div>
     );
-
-  if (!project) {
-    return (
-      <div className="flex items-center justify-center h-screen">
-        <div className="w-12 h-12 border-4 border-black border-dashed rounded-full animate-spin"></div>
-      </div>
-    );
-  }
 
   function clickThumbnail() {
     setIsClicked((click) => !click);
